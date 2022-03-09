493cc097670ffd3c5af16c54a45631fd5fc5ffa9
Manwar Sabaeai <46737535+Manwar-S@users.noreply.github.com>
2022-03-09 14:00:51 -0800

    Add files via upload
    The 'AppInstaller' folder would usually has MSI files, but couldn't upload them since GitHub only allow files that are smaller than 25M

---
+++ AppInstaller/AppInstaller.ps1
@@ -0,0 +1,28 @@
+﻿$appList = @(Get-Content -Path 'C:\Lab\Apps\Apps.txt' )
+$installedApps = Get-ItemProperty @(
+            'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+            'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*')
+[bool]$installed = $false
+$msiFile = "C:\Lab\Apps"
+$msiArgs = "-qb"
+
+foreach($app in $appList){
+   foreach($installedApp in $installedApps){
+      if($installedApp.DisplayName -like "*$app*"){
+         $installed = $true
+         echo "$app Already Installed"
+      }   
+   }
+        if($installed -eq $false){
+                echo "$app Isn't Installed"
+                echo "Wait......" 
+                $installer = @(Get-ChildItem $msiFile -Name)
+                foreach($install in $installer){
+                    if($install -like "*$app*"){
+                        (Start-Process -FilePath "$install" -WorkingDirectory $msiFile -ArgumentList $msiArgs -Wait -Passthru).ExitCode
+                        echo "$app Installed!!!"    
+                    }                             
+                }                
+        }
+   $installed = $false
+}
